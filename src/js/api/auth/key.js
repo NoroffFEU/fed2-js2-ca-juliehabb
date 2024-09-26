6e70b9806543b04dbfb661c28f2642d899ace932
juliehabb <julie.habbestad@hotmail.com>
2024-09-26 12:59:43 +0200

    Save to local storage

--- src/js/api/auth/key.js
+++ src/js/api/auth/key.js
@@ -1,1 +1,18 @@
-export async function getKey(name) {}
+
+export function save(key,value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+export async function load(key) {
+    try {
+        const value = localStorage.getItem(key);
+        return JSON.parse(value);
+
+    } catch {
+        return null;
+    }
+}
+
+export function remove(key) {
+    localStorage.removeItem(key);
+}
