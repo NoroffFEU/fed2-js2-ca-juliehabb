2e6a6a54fbbd2a3bf33ee84f70d3f7ba8703e5da
juliehabb <julie.habbestad@hotmail.com>
2024-10-01 11:54:31 +0200

    Delete post

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,22 @@
-export async function deletePost(id) {}
+import { API_SOCIAL_POSTS } from "../constants.js";
+import { authFetch } from "../authFetch.js";
+
+export async function deletePost(id) {
+
+    const deletePostUrl = `${API_SOCIAL_POSTS}/${id}`
+
+    if(!id) {
+        throw new error ("delete requires post ID")
+    }
+
+    const response = await authFetch(deletePostUrl, {
+        method: "delete",
+    });
+
+    return await response.json();
+
+}
+
+deletePost(2122);
+
+
