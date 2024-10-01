496a9a0c3a75306724976e961b4fe5e0166e52e9
juliehabb <julie.habbestad@hotmail.com>
2024-10-01 11:54:53 +0200

    Update post

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,1 +1,35 @@
-export async function updatePost(id, { title, body, tags, media }) {}
+import { authFetch } from "../authFetch.js";
+import { API_SOCIAL_POSTS } from "../constants.js";
+
+export async function updatePost(id, { title, body, tags, media }) {
+
+
+    const updatePostUrl = `${API_SOCIAL_POSTS}/${id}`
+
+    if(!id) {
+        throw new error ("update requires post ID")
+    }
+
+    const response = await authFetch(updatePostUrl, {
+        method: "put",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title,
+            body,
+            tags,
+            media,
+        }),
+    });
+
+    const updatedPost =  await response.json();
+
+    console.log(updatedPost);
+}
+
+updatePost(2122, {
+    title: "updated example title",
+    body: "example updated",
+})
+
