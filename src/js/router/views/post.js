97cef1716c40b2faff3b08e4c47cfe9de0ec89c7
juliehabb <julie.habbestad@hotmail.com>
2024-10-02 09:45:52 +0200

    Render posts on page

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,1 +1,64 @@
+import { readPost } from "../../api/post/read.js";
+
 alert("Single Post Page");
+
+function renderPost(title, body, media, tags, container, maxLength = 250) {
+    const blogPostBlockElement = document.createElement("div");
+    blogPostBlockElement.classList.add("post-container");
+
+    const articleElement = document.createElement("article");
+
+    // Create media element (image) if media is available
+    const mediaElement = document.createElement("img");
+    if (media) {
+        mediaElement.src = media;
+        mediaElement.classList.add("post-img");
+    }
+    const textContentElement = document.createElement("div");
+    textContentElement.classList.add("t-content");
+
+    const titleElement = document.createElement("h1");
+    titleElement.textContent = title;
+    titleElement.classList.add("post-title");
+
+    // Create and define the body element
+    const bodyElement = document.createElement("p");
+    bodyElement.textContent = body;
+    bodyElement.classList.add("post-body");
+
+    const tagContainerElement = document.createElement("div");
+    tagContainerElement.classList.add("tag-marker");
+
+    const tagsElement = document.createElement("p");
+    tagsElement.textContent = tags.join(", ");
+    
+    blogPostBlockElement.appendChild(articleElement);
+    if (media) {
+        articleElement.appendChild(mediaElement);
+    }
+    articleElement.appendChild(textContentElement);
+    textContentElement.appendChild(titleElement);
+    textContentElement.appendChild(bodyElement);
+    textContentElement.appendChild(tagContainerElement);
+    tagContainerElement.appendChild(tagsElement);
+
+    container.appendChild(blogPostBlockElement);
+}
+
+export async function createPostDisplay() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    const post = await readPost(id);
+
+    const container = document.querySelector(".post-container")
+
+    const title = post.title;
+    const body = post.body;
+    const media = post.media ? post.media : null;
+    const tags = post.tags.length > 0 ? post.tags : ["No tags"];
+
+    renderPost(id, title, body, media, tags, container, 200 )
+}
+
+createPostDisplay()
--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,3 +1,81 @@
-import { authGuard } from "../../utilities/authGuard";
+//import { authGuard } from "../../utilities/authGuard";
+import { readPosts } from "../../api/post/read.js";
 
-authGuard();
+//authGuard();
+
+function renderPosts(id, title, body, media, tags, container, maxLength = 250) {
+    const blogPostBlockElement = document.createElement("div");
+    blogPostBlockElement.classList.add("post-container");
+
+    const cardContainerElement = document.createElement("div");
+    cardContainerElement.classList.add("card-container");
+
+    const verticalCardElement = document.createElement("a");
+    verticalCardElement.classList.add("vertical-card");
+    verticalCardElement.href = "./post/index.html?id=" + id;
+
+    const articleElement = document.createElement("article");
+
+    // Create media element (image) if media is available
+    const mediaElement = document.createElement("img");
+    if (media) {
+        mediaElement.src = media;
+        mediaElement.classList.add("post-img");
+    }
+
+    const cardContentElement = document.createElement("div");
+    cardContentElement.classList.add("v-card-content");
+
+    const titleElement = document.createElement("h4");
+    titleElement.textContent = title;
+    titleElement.classList.add("card-title");
+
+    // Create and define the body element
+    const bodyElement = document.createElement("p");
+    bodyElement.textContent = body.length > maxLength ? body.slice(0, maxLength) + "..." : body;
+    bodyElement.classList.add("post-body");
+
+    const tagContainerElement = document.createElement("div");
+    tagContainerElement.classList.add("tag-marker");
+
+    const tagsElement = document.createElement("p");
+    tagsElement.textContent = tags.join(", ");
+    
+    // Append all elements to the DOM
+    blogPostBlockElement.appendChild(cardContainerElement);
+    cardContainerElement.appendChild(verticalCardElement);
+    verticalCardElement.appendChild(articleElement);
+    if (media) {
+        articleElement.appendChild(mediaElement);
+    }
+    articleElement.append(cardContentElement);
+    cardContentElement.appendChild(titleElement);
+    cardContentElement.appendChild(bodyElement);
+    cardContentElement.appendChild(tagContainerElement);
+    tagContainerElement.appendChild(tagsElement);
+
+    container.appendChild(cardContainerElement);
+}
+
+export async function createPostsDisplay() {
+    const posts = await readPosts();
+
+    const latestPosts = posts.slice(0,12);
+
+    const container = document.querySelector(".card-container")
+
+    latestPosts.forEach(post => {
+        if (post.media) {
+            const title = post.title;
+            const id = post.id;
+            const body = post.body;
+            const media = post.media ? post.media : null;
+            const tags = post.tags.length > 0 ? post.tags : ["No tags"];
+
+            renderPosts(id, title, body, media, tags, container, 200 )
+        }
+    });
+
+}
+
+createPostsDisplay()
