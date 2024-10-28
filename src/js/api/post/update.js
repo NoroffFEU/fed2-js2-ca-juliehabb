7a903b19be5a2abd0e25207f66b0be8c58d1d431
juliehabb <julie.habbestad@hotmail.com>
2024-10-28 16:17:39 +0100

    Added documentation

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,8 +1,37 @@
 import { authFetch } from "../authFetch.js";
 import { API_SOCIAL_POSTS } from "../constants.js";
 
+/**
+ * Updates an existing post in the API with new data.
+ * 
+ * @async
+ * @function updatePost
+ * @param {string} id - The ID of the post to update.
+ * @param {Object} postDetails - The new details of the post.
+ * @param {string} postDetails.title - The new title of the post.
+ * @param {string} postDetails.body - The new body content of the post.
+ * @param {Array<string>} postDetails.tags - An array of tags for the post.
+ * @param {Array<string>} [postDetails.media] - An optional array of media URLs associated with the post.
+ * @returns {Promise<Object>} A promise that resolves to the updated post object.
+ * 
+ * @throws {Error} Throws an error if the post ID is not provided.
+ * 
+ * @example
+ * // Example usage:
+ * updatePost('12345', {
+ *   title: 'Updated Post Title',
+ *   body: 'This is the updated body of the post.',
+ *   tags: ['update', 'example'],
+ *   media: ['https://example.com/image.jpg']
+ * })
+ * .then(updatedPost => {
+ *   console.log('Post updated:', updatedPost);
+ * })
+ * .catch(error => {
+ *   console.error('Error updating post:', error);
+ * });
+ */
 export async function updatePost(id, { title, body, tags, media }) {
-
 
     const updatePostUrl = `${API_SOCIAL_POSTS}/${id}`
 
@@ -25,11 +54,5 @@ export async function updatePost(id, { title, body, tags, media }) {
 
     const updatedPost =  await response.json();
 
-    console.log(updatedPost);
+    return updatedPost;
 }
-
-updatePost(2122, {
-    title: "updated example title",
-    body: "example updated",
-})
-
