158b773fa30659aee7f3af186a62f108f96582be
juliehabb <julie.habbestad@hotmail.com>
2024-10-28 16:15:38 +0100

    Added documentation

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,6 +1,26 @@
 import { API_SOCIAL_POSTS } from "../constants.js";
 import { authFetch } from "../authFetch.js";
 
+/**
+ * Deletes a post from the social API by its ID.
+ * 
+ * @async
+ * @function deletePost
+ * @param {string} id - The ID of the post to be deleted.
+ * @returns {Promise<Object|undefined>} A promise that resolves to the response data if the post is deleted, or undefined if the deletion is successful with no content.
+ * 
+ * @throws {Error} Throws an error if the post ID is not provided.
+ * 
+ * @example
+ * // Example usage:
+ * deletePost('12345')
+ *   .then(response => {
+ *     console.log('Post deleted:', response);
+ *   })
+ *   .catch(error => {
+ *     console.error('Error deleting post:', error);
+ *   });
+ */
 export async function deletePost(id) {
 
     const deletePostUrl = `${API_SOCIAL_POSTS}/${id}`
@@ -10,13 +30,16 @@ export async function deletePost(id) {
     }
 
     const response = await authFetch(deletePostUrl, {
-        method: "delete",
+        method: "DELETE",
     });
 
-    return await response.json();
+    if (response.status === 204) {
+        return;
+    }
+
+    const responseData = await response.json();
+    return responseData;
 
 }
-
-deletePost(2122);
 
 
